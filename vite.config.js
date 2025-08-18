e69f415274df83e3c9e58de426e51c44afad1307
Ian Ale <ianalebom@gmail.com>
2025-08-18 15:10:18 +0700

    template

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,28 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: ['resources/js/app.tsx',
+                'resources/css/app.css',
+                'resources/css/highlight.min.css',
+                'resources/css/easymde.min.css',
+                'resources/css/fancybox.css',
+                'resources/css/flatpickr.min.css',
+                'resources/css/font-awesome.min.css',
+                'resources/css/fullcalendar.min.css',
+                'resources/css/highlight.min.css',
+                'resources/css/markdown-editor.css',
+                'resources/css/nice-select.css',
+                'resources/css/nice-select2.css',
+                'resources/css/nouislider.min.css',
+                'resources/css/quill.snow.css',
+                'resources/css/swiper-bundle.min.css',
+                'resources/css/tippy.css',
+            ],
             refresh: true,
         }),
         react(),
